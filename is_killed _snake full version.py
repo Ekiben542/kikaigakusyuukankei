b42d8814aa1a3f250642bd6ad2be26c84c6b2222
Ekiben542 <71870048+Ekiben542@users.noreply.github.com>
2024-06-16 18:44:48 +0900

    Update is_killed _snake full version.py

--- is_killed _snake full version.py
+++ is_killed _snake full version.py
@@ -118,11 +118,7 @@ def calculate_distance(pt1, pt2):
 def is_snake_killed(contour, img):
     mask = np.zeros(img.shape[:2], dtype=np.uint8)
     cv2.drawContours(mask, [contour], -1, 255, thickness=cv2.FILLED)
-   
-    # Create a mask for white areas in the image
     white_mask = ((img[:, :, 0] > 200) & (img[:, :, 1] > 200) & (img[:, :, 2] > 200)).astype(np.uint8) * 255
-   
-    # Perform bitwise_and with uint8 mask
     white_area = cv2.countNonZero(cv2.bitwise_and(mask, mask, mask=white_mask))
    
     return white_area > 0
