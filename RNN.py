f994572f90f4860185213e6d50d33bbf2b95b04f
Ekiben542 <71870048+Ekiben542@users.noreply.github.com>
2024-06-16 17:20:27 +0900

    Update RNN.py
    あ

--- RNN.py
+++ RNN.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
-from scipy.special import expit  # Sigmoid function
+from scipy.special import expit  # Sigmoid 
 
 class SnakeAI:
     def __init__(self, state_size, action_size):
